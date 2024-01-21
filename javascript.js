5a5aa77a51e3c892c31dcb4e251abf0589eaab07
vaibhavsraut <vaibhav.raut851@gmail.com>
2024-01-21 16:42:20 +0530

    Program to check if given year is Leap

--- javascript.js
+++ javascript.js
@@ -1,22 +1,11 @@
-function dayofmonth(){
-    var day = parseInt(prompt("Enter the day of the month:"));
-    var month = parseInt(prompt("Enter the month (1-12):"));
+function isLeapYear(){
+    let year = parseInt(prompt("Enter the year:"));
 
-    if(month>=3 && month<=6){
-        if(month == 3){
-            if(day>=20){
-                console.log(true);
-            }
-        }else if(month == 6){
-            if(day<=20){
-                console.log(true);
-            }
-        }else{
-            console.log(true);
-        }
-    }else{
-        console.log(false);
+    if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+        console.log("Leap Year");
+    }else {
+        console.log("Not Leap Year");
     }
 
 }
-dayofmonth();
+isLeapYear();
